030ea25ab0d6941ddf17f06334aced2fd4927f7e
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-09-03 16:22:06 +0200

    Added stylings

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,15 +3,26 @@ import api from "../api";
 
 const SideBar: NextPage = async ({}) => {
     const results = await api.get("menus/");
-    let menus = [];
+    let menu_links = [];
     for (let result of results.data) {
-        menus.push(
-            <a href={`#${result.id}`} className="block text-cyan-300">
+        menu_links.push(
+            <a
+                href={`#menu-${result.id}`}
+                className="text-cyan-800 flex items-center ml-3 shadow-sm shadow-blue-200"
+                key={result.id}
+            >
+                <img
+                    src="https://icons.iconarchive.com/icons/icons8/ios7/48/Food-Cutlery-icon.png"
+                    alt=""
+                    className="my-3"
+                    width={30}
+                />
                 {result.name}
             </a>
         );
     }
-    return <nav className="w-2/6">{menus}</nav>;
+
+    return <nav className="w-1/6 bg-slate-300">{menu_links}</nav>;
 };
 
 export default SideBar;
