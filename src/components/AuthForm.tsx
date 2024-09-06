afcaa01f6b6915d47065e85c29e8a1a68706aff2
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-09-06 10:35:37 +0200

    Add AuthForm component

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,19 @@
+import { HTTPMethods } from "@/types";
+
+const AuthForm = function (
+    { action }: { action: string },
+    { method }: { method: string }
+) {
+    return (
+        <form action={action} method={method}>
+            <input type="text" name="username" />
+            <input type="password" name="password" />
+            <button
+                type="submit"
+                className="text-emerald-500 bg-blackr rounded-md p-2"
+            >
+                Login
+            </button>
+        </form>
+    );
+};
