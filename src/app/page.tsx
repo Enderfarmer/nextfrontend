f6b8b82117c9d57eb305cf4de6c804f39335d09d
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-09-06 10:09:47 +0200

    Make home client and eliminate stupid type errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,22 @@
 "use client";
 import "@/styles/Home.css";
 import api from "@/api";
-import SideBar from "@/components/SideBar";
-import { MenuEntries, MenuEntry, Menu } from "@/types";
+import Contents from "./contents";
+import { useEffect, useState } from "react";
 
-function Meal({
-    meal,
-    entries,
-}: {
-    meal: keyof MenuEntry;
-    entries: MenuEntries;
-}) {
-    return (
-        <tr>
-            <td className="heading">{meal}</td>
-            <td>{entries.monday?.[meal] || "Meal not specified"}</td>
-            <td>{entries.tuesday?.[meal] || "Meal not specified"}</td>
-            <td>{entries.wednesday?.[meal] || "Meal not specified"}</td>
-            <td>{entries.thursday?.[meal] || "Meal not specified"}</td>
-            <td>{entries.friday?.[meal] || "Meal not specified"}</td>
-            <td>{entries.saturday?.[meal] || "Meal not specified"}</td>
-            <td>{entries.sunday?.[meal] || "Meal not specified"}</td>
-        </tr>
-    );
-}
-
-export default async function Home() {
-    const response = await api.get("menus/", {
-        headers: {
-            Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI1NTUzOTIxLCJpYXQiOjE3MjU1NDY3MjEsImp0aSI6IjRkYTEwMTE1MjRlNzRmN2M4M2Q1ODkyZDFiM2ViMzUyIiwidXNlcl9pZCI6Mn0.cKgiW7kvOGru3mumS_kITrrmO7gWK6HKprDbypmnRuw",
-        },
-    });
-    const results: Array<Menu> = response.data;
-
-    return (
-        <main className="flex w-full">
-            <div className="w-5/6">
-                {results.map((menu) => (
-                    <div id={`menu-${menu.id}`} key={menu.id} className="menu">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <td className="heading">Mealtime</td>
-                                    <td className="heading">Monday</td>
-                                    <td className="heading">Tuesday</td>
-                                    <td className="heading">Wednesday</td>
-                                    <td className="heading">Thursday</td>
-                                    <td className="heading">Friday</td>
-                                    <td className="heading">Saturday</td>
-                                    <td className="heading">Sunday</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <Meal meal="breakfast" entries={menu.entries} />
-                                <Meal meal="lunch" entries={menu.entries} />
-                                <Meal meal="dessert" entries={menu.entries} />
-                                <Meal meal="dinner" entries={menu.entries} />
-                            </tbody>
-                        </table>
-                    </div>
-                ))}
-            </div>
-            <SideBar menus={results} />
-        </main>
-    );
+export default function Home() {
+    let [data, setData] = useState(null);
+    let [loading, setLoading] = useState(true);
+    useEffect(() => {
+        api.get("http://127.0.0.1:8000/api/menus/", {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+            },
+        }).then((res) => {
+            setLoading(false);
+            setData(res.data);
+        });
+    }, []);
+    if (loading) return <main className="text-center">Loading...</main>;
+    return <Contents data={data} />;
 }
