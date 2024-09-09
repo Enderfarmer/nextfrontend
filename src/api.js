be030e678f09b8e49e1128eb8c37aeff4d1dc76b
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-09-09 15:10:22 +0200

    Add authenticated GET & POST

--- src/api.js
+++ src/api.js
@@ -1,6 +1,29 @@
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
-const api = axios.create({
+let api = axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
 });
+api.authget = async function (url, config, accesstoken, refresh) {
+    let conf = config;
+    if (jwtDecode(accesstoken).exp < Date.now()) {
+        accesstoken = (await this.post("token/refresh/", { refresh: refresh }))
+            .data.access;
+    }
+    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
+    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
+    else conf.headers = { Authorization: `Bearer ${accesstoken}` };
+    return await this.get(url, conf);
+};
+api.authpost = async function (url, data, config, accesstoken, refresh) {
+    let conf = config;
+    if (jwtDecode(accesstoken).exp < Date.now()) {
+        accesstoken = (await this.post("token/refresh/", { refresh: refresh }))
+            .data.access;
+    }
+    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
+    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
+    else conf.headers = { Authorization: `Bearer ${accesstoken}` };
+    return await this.post(url, data, conf);
+};
 export default api;
