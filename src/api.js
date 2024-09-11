8aa88287826def37fcf101e2c27d7d04cd353724
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-09-11 17:59:12 +0200

    Write a special class with useful methods for the api

--- src/api.js
+++ src/api.js
@@ -1,35 +1,40 @@
-import axios from "axios";
+import { Axios } from "axios";
 import { jwtDecode } from "jwt-decode";
 import { redirect } from "next/navigation";
 
-let api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
-});
-api.authget = async function (url, config, accesstoken, refresh) {
-    let conf = config;
-    if (jwtDecode(accesstoken).exp < Date.now()) {
-        accesstoken = (await this.post("token/refresh/", { refresh: refresh }))
-            .data.access;
+class Api extends Axios {
+    constructor(baseURL) {
+        this.baseURL = baseURL;
     }
-    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
-    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
-    else conf.headers = { Authorization: `Bearer ${accesstoken}` };
-    return await this.get(url, conf);
-};
-api.authpost = async function (url, data, config, accesstoken, refresh) {
-    let conf = config;
-    if (jwtDecode(accesstoken).exp < Date.now()) {
-        try {
+
+    async authget(url, config, accesstoken, refresh) {
+        let conf = config;
+        if (jwtDecode(accesstoken).exp < Date.now()) {
             accesstoken = (
                 await this.post("token/refresh/", { refresh: refresh })
             ).data.access;
-        } catch (err) {
-            redirect("/login/");
         }
+        if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
+        else conf.headers = { Authorization: `Bearer ${accesstoken}` };
+        return await this.get(url, conf);
     }
-    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
-    if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
-    else conf.headers = { Authorization: `Bearer ${accesstoken}` };
-    return await this.post(url, data, conf);
-};
+    async authpost(url, data, config, accesstoken, refresh) {
+        let conf = config;
+        if (jwtDecode(accesstoken).exp < Date.now()) {
+            try {
+                accesstoken = (
+                    await this.post("token/refresh/", { refresh: refresh })
+                ).data.access;
+            } catch (err) {
+                redirect("/login/");
+            }
+        }
+        if (conf.headers) conf.headers.Authorization = `Bearer ${access}`;
+        else conf.headers = { Authorization: `Bearer ${accesstoken}` };
+        return await this.post(url, data, conf);
+    }
+}
+
+let api = new Api("http://127.0.0.1:8000/api/");
+
 export default api;
