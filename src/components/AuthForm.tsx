f61cb38c7350ca3183846e3ea67b000f7898b6da
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-10-04 19:18:04 +0200

    Add better styling

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -13,24 +13,41 @@ const AuthForm = function ({
         action(formData); // Pass the form data to the action function
     };
     return (
-        <main>
-            <form onSubmit={handleSubmit} method={method || "POST"}>
-                <h1>{auth}</h1>
+        <main className="flex items-center justify-center border h-28">
+            <form
+                onSubmit={handleSubmit}
+                method={method || "POST"}
+                className="px-5 flex flex-col justify-center items-center"
+            >
+                <h1 className="font-extrabold p-2 text-3xl">{auth}</h1>
                 <input
                     type="text"
                     name="username"
-                    className="border-emerald-400 border"
+                    className="border-emerald-400 border rounded p-2 text-lg"
+                    spellCheck="false"
                 />
                 <br />
                 <input
                     type="password"
                     name="password"
-                    className="border-emerald-400 border"
+                    className="border-emerald-400 border rounded p-2 text-lg"
+                    spellCheck="false"
                 />
+                <br />
+                {auth === "Register" && (
+                    <>
+                        <input
+                            type="password"
+                            name="password2"
+                            className="border-emerald-400 border rounded p-2 text-lg"
+                            spellCheck="false"
+                        />
+                    </>
+                )}
                 <br />
                 <button
                     type="submit"
-                    className="text-emerald-500 bg-blackr rounded-md p-2"
+                    className="text-emerald-300 bg-black rounded-md p-2 text-lg w-11/12"
                 >
                     {auth}
                 </button>
