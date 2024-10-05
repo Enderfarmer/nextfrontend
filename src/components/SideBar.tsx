398b79e12ebe55e79397744671c21ea480f32cc9
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-10-05 17:31:29 +0200

    Add deleting and enhance style

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,18 +1,20 @@
+import api from "@/api";
 import { Menu } from "@/types";
 import Link from "next/link";
-import { BiFoodMenu, BiLogOut } from "react-icons/bi";
+import { BiFoodMenu, BiLogOut, BiTrash } from "react-icons/bi";
 import { FaPlus, FaPencilAlt } from "react-icons/fa";
 const SideBar = ({
     menus = null,
 }: {
     menus: Array<Menu> | undefined | null;
 }) => {
+    let ICON_SIZE = "1.25em";
     let menu_links = [];
     if (menus) {
         for (let result of menus) {
             menu_links.push(
                 <div className="flex text-center items-center" key={result.id}>
-                    <BiFoodMenu className="w-7 text-black" width={30} />
+                    <BiFoodMenu className="text-black" size={ICON_SIZE} />
                     <a
                         href={`#menu-${result.id}`}
                         className="text-cyan-800 flex items-center ml-3 shadow-sm shadow-blue-200"
@@ -20,12 +22,25 @@ const SideBar = ({
                     >
                         {result.name}
                     </a>
-                    <Link href={`/edit/${result.id}`}>
-                        <FaPencilAlt
-                            className="w-7 text-black hover:text-blue-950"
-                            width={30}
-                        />
+                    <Link
+                        href={`/edit/${result.id}`}
+                        title="Edit menu"
+                        className="text-black hover:text-blue-800 hover:scale-125 m-2"
+                    >
+                        <FaPencilAlt size={ICON_SIZE} />
                     </Link>
+                    <BiTrash
+                        size={ICON_SIZE}
+                        className="text-red-700 hover:text-red-500  hover:scale-125"
+                        onClick={function (e) {
+                            api.authdelete(
+                                `menus/${result.id}/`,
+                                {},
+                                localStorage
+                            );
+                        }}
+                        title="Delete menu"
+                    />
                 </div>
             );
         }
